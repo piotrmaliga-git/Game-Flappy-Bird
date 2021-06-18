eb17937743c3e5a29847396388a4b8fc227dedb8
Piotr Maliga <piotr.maliga15813@gmail.com>
2021-06-18 19:36:39 +0200

    Added moving floor

--- game.py
+++ game.py
@@ -13,6 +13,16 @@ clock = pygame.time.Clock()
 background = pygame.image.load('img/background.png').convert()
 background = pygame.transform.scale2x(background)
 
+#base
+base = pygame.image.load('img/base.png').convert()
+base = pygame.transform.scale2x(base)
+base_x_pos = 0
+
+def draw_base():
+    screen.blit(base,(base_x_pos,900))
+    screen.blit(base,(base_x_pos + 576,900))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,6 +30,10 @@ while True:
             sys.exit()
 
     screen.blit(background,(0,0))
+    base_x_pos -= 1
+    draw_base()
+    if base_x_pos<= -576:
+        base_x_pos = 0
 
     pygame.display.update()
     clock.tick(120)
