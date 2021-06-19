933ec16856d53e28ab1893535bfca7339dced4da
Piotr Maliga <piotr.maliga15813@gmail.com>
2021-06-19 16:16:49 +0200

    Added collisions

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ clock = pygame.time.Clock()
 # game variables
 gravity = 0.25
 bird_movement = 0
+game_active = True
 
 # background
 background_surface = pygame.image.load('img/background.png').convert()
@@ -64,28 +65,45 @@ def draw_pipes(pipes):
             flip_pipe = pygame.transform.flip(pipe_surface, False, True)
             screen.blit(flip_pipe, pipe)
 
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        return False
+    return True
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key ==pygame.K_w:
+            if (event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key ==pygame.K_w) and game_active:
                 bird_movement = 0
-                bird_movement -= 12
+                bird_movement -= 8
+            if  (event.key == pygame.K_SPACE or event.key == pygame.K_UP or event.key ==pygame.K_w) and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100,512)
+                bird_movement = 0
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipes())
 
     screen.blit(background_surface,(0,0))
 
-    # bird
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface,bird_rect)
+    if game_active:
+        # bird
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface,bird_rect)
 
-    # pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        # collision
+        game_active= check_collision(pipe_list)
+
+        # pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
 
     # base
     base_x_pos -= 1
