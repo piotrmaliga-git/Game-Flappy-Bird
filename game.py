31ef652db18eb027e75334076b9a8ae911a5e5c6
Piotr Maliga <piotr.maliga15813@gmail.com>
2021-06-20 15:46:34 +0200

    Adding a message

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ high_score = 0
 background_surface = pygame.image.load('img/background.png').convert()
 background_surface = pygame.transform.scale2x(background_surface)
 
+
 # base
 base_surface = pygame.image.load('img/base.png').convert()
 base_surface = pygame.transform.scale2x(base_surface)
@@ -49,6 +50,9 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE,1200)
 
 pipe_height = [400, 600, 800]
+
+game_over_surface = pygame.transform.scale2x(pygame.image.load('img/message.png').convert_alpha())
+game_over_rect =game_over_surface.get_rect(center =(288,512))
 
 def create_pipes():
     random_pipe_pos = random.choice(pipe_height)
@@ -149,6 +153,7 @@ while True:
         score  += 0.01
         score_display('main_game')
     else:
+        screen.blit(game_over_surface, game_over_rect)
         high_score = update_score(score, high_score)
         score_display('game_over')
 
